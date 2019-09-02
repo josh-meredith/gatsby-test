e513f1c8223a23345fc9b1472d3f4fe9730de34d
Josh Meredith <meredith.josh@gmail.com>
2019-09-02 21:19:04 +0800

    Add wordpress source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,27 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: "wp-api.joshmeredith.com.au",
+        protocol: "http",
+        hostingWPCOM: false,
+        useACF: true,
+        acfOptionPageIds: [],
+        auth: {},
+        verboseOutput: true,
+        perPage: 100,
+        concurrentRequests: 5,
+        includedRoutes: [
+          "**/*/*/categories",
+          "**/*/*/posts",
+          "**/*/*/pages",
+          "**/*/*/media",
+          "**/*/*/menus",
+          "**/*/*/attachments",
+        ],
+      },
+    },
   ],
 }
